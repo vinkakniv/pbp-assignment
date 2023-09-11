07fc2e7a5878be5ca1bf0a441b9a1b936f44ed31
vinkakniv <vinka.aeris@gmail.com>
2023-09-11 13:00:56 +0700

    add testing

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from main.models import Item
 
-# Create your tests here.
+
+class mainTest(TestCase):
+    def setUp(self):
+        Item.objects.create(name="Good day", amount="8", description="Funtastic Mocaccino Coffee")
+        Item.objects.create(name="Fanta", amount=7, description="Orange flavoured")
+
+    def test_item_is_in_stock(self):
+        """Items in vending machine that correctly identified"""
+        good_day = Item.objects.get(name="Good day")
+        fanta = Item.objects.get(name="Fanta")
+        self.assertEqual(good_day.description, "Funtastic Mocaccino Coffee")
+        self.assertEqual(fanta.amount, 7)
